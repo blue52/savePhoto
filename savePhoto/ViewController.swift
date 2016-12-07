65c0caecbf2022850dc208d6de4de89c0bd986d5
sky <bluesky@skydemacbook-pro.local>
2016-12-07 23:09:32 +0800

    savephoto

--- savePhoto/ViewController.swift
+++ savePhoto/ViewController.swift
@@ -8,17 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let imagePicker = UIImagePickerController()
+    var newphotoArray:[String] = []
+
+    @IBOutlet weak var showPhoto: UIImageView!
+    
+    @IBAction func takephotoButton(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadfile()
+        let path = NSHomeDirectory()//為了印出路徑
+        print(path)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        //print("info \(info)")
+        let image = info[UIImagePickerControllerOriginalImage]
+        self.showPhoto.image = image as? UIImage
+        //取得路徑
+        let fileManager = FileManager.default
+        let docUrls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let docUrl = docUrls.first
+        //檔名
+        let interval = Date.timeIntervalSinceReferenceDate
+        let name = "\(interval).jpg"
+        let url = docUrl?.appendingPathComponent(name)
+        print(url!)
+        //把圖片存在App裡
+        let data = UIImageJPEGRepresentation(self.showPhoto.image!, 0.9)
+        try! data?.write(to: url!)
+        self.dismiss(animated: true, completion: nil)
+        newphotoArray.append(name)
+        savefile()
+        
+        
+    }
+    
+    //儲存紀錄位址的photoArray
+    func savefile(){
+        
+        let fileManager = FileManager.default
+        let docUrls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let docUrl = docUrls.first
+        let url = docUrl?.appendingPathComponent("newphotoArray.txt")
+        let arrary = newphotoArray
+        (arrary as NSArray).write(to: url!, atomically: true)
+        
+    }
+    //讀取記錄位址的photoArray
+    func loadfile(){
+        let fileManager = FileManager.default
+        let docUrls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let docUrl = docUrls.first
+        let url = docUrl?.appendingPathComponent("newphotoArray.txt")
+        
+        if let array = NSArray(contentsOf: url!){
+            newphotoArray = array as! [String]
+        }// if array
+        //print(newphotoArray)
+    }//loadfile
 
 
 }
